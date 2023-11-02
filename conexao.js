94da70d937204ad77d7039aa49b910d15e4a4baf
DouglasRochass <124200549+DouglasRochass@users.noreply.github.com>
2023-11-02 18:28:32 -0300

    Update conexao.js

--- conexao.js
+++ conexao.js
@@ -1,9 +1,15 @@
 const Sequelize = require('sequelize');
 
 
-const sequelize = new Sequelize('cadastro_aluno', 'root', 'Douglas5', {
-    host: 'localhost',
-    dialect: 'mysql'
+const sequelize = new Sequelize('cadastro_aluno', 'Douglas', 'batata5.', {
+    host: 'museus.mysql.database.azure.com',
+    dialect: 'mysql',
+    "ssl": true,
+    "dialectOptions": {
+       "ssl": {
+          "require": true
+       }
+     }
   });
 
 module.exports = sequelize
